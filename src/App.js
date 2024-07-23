9a6618a298942263f92c2d155a9cb34b624d6578
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-07-23 15:58:05 +0700

    Add Reason

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Hero from './component/Hero/Hero'
 import Programs from './component/Programs/Programs'
+import Reason from './component/Reasons/Reason'
 function App() {
   return (
     <div className="App">
       <Hero />
       <Programs />
+      <Reason />
     </div>
   );
 }
