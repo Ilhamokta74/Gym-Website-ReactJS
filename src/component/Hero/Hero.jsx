010a57b6fc6550bb7a8b1cb8f138e69324fcd13c
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-07-23 14:49:36 +0700

    Fix Header

--- src/component/Hero/Hero.jsx
+++ src/component/Hero/Hero.jsx
@@ -3,17 +3,81 @@ import Header from '../Header/Header'
 
 import './Hero.css'
 
+import hero_image from '../../assets/hero_image.png';
+import hero_image_back from '../../assets/hero_image_back.png';
+import heart from '../../assets/heart.png';
+import calories from '../../assets/calories.png';
+
 const Hero = () => {
     return (
         <div className="hero">
             <div className="left-h">
-                <Header/>
+                <Header />
+                {/* The Best Ad */}
                 <div className="the-best-ad">
                     <div></div>
                     <span>the best fitness in the town</span>
                 </div>
+
+                {/* Hero Heading */}
+                <div className="hero-text">
+                    <div>
+                        <span className='stroke-text'>Shape </span>
+                        <span>Your</span>
+                    </div>
+                    <div>
+                        <span>Ideal Body</span>
+                    </div>
+                    <div>
+                        <span>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis velit eos possimus veniam ea autem!</span>
+                    </div>
+                </div>
+
+                {/* Figures */}
+                <div className="figures">
+                    <div>
+                        <span>+140</span>
+                        <span>Expert Coachs</span>
+                    </div>
+                    <div>
+                        <span>+978</span>
+                        <span>Members Joined</span>
+                    </div>
+                    <div>
+                        <span>+50</span>
+                        <span>Fitness Programs</span>
+                    </div>
+                </div>
+
+                {/* Hero Button */}
+                <div className="hero-button">
+                    <button className='btn'>Get Started</button>
+                    <button className='btn'>Learn More</button>
+                </div>
             </div>
-            <div className="right-h">Right Side</div>
+
+            <div className="right-h">
+                <button className='btn'>Join Now</button>
+
+                <div className="heart-rate">
+                    <img src={heart} alt="" />
+                    <span>Heart Rate</span>
+                    <span>116 bpm</span>
+                </div>
+
+                {/* Hero Images */}
+                <img src={hero_image} alt="" className='hero-image' />
+                <img src={hero_image_back} alt="" className='hero-image-back' />
+
+                {/* Calories */}
+                <div className="calories">
+                    <img src={calories} alt="" />
+                    <div>
+                        <span>Calories Burned</span>
+                        <span>220 kcal</span>
+                    </div>
+                </div>
+            </div>
 
         </div>
     )
---
+++ src/component/Programs/Programs.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+import './Programs.css'
+
+const Programs = () => {
+    return (
+        <div className="Programs" id="programs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sint dolorem molestias unde laborum perspiciatis.</div>
+    )
+}
+
+export default Programs;
