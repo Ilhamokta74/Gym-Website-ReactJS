694941bbe0217b23a528a45f2d9a1e4910b7cc0e
Ilham Oktavian <99379666+Ilhamokta74@users.noreply.github.com>
2024-07-23 15:15:19 +0700

    Add Section Program

--- src/component/Programs/Programs.jsx
+++ src/component/Programs/Programs.jsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import { programsData } from '../../data/programsData'
+import RightArrow from '../../assets/rightArrow.png'
 
 import './Programs.css'
 
 const Programs = () => {
     return (
-        <div className="Programs" id="programs">Lorem ipsum dolor sit amet consectetur adipisicing elit. Maiores sint dolorem molestias unde laborum perspiciatis.</div>
+        <div className="Programs" id="programs">
+            {/* Header */}
+            <div className="programs-header">
+                <span className='stroke-text'>Explore Our</span>
+                <span>Programs</span>
+                <span className='stroke-text'>To Shape you</span>
+            </div>
+
+            <div className="program-categories">
+                {programsData.map((program) => (
+                    <div className="category">
+                        {program.image}
+                        <span>{program.heading}</span>
+                        <span>{program.details}</span>
+                        <div className="join-now">
+                            <span>Join Now</span>
+                            <img src={RightArrow} alt="" />
+                        </div>
+                    </div>
+                ))}
+            </div>
+        </div>
     )
 }
 
